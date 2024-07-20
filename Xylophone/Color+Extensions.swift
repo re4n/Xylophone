c1132e5994c64368d002cb3fce8b6614e9f31052
Ryan Rouxinol <ryanrouxinol17@gmail.com>
2024-07-19 21:46:57 -0300

    first commit

---
+++ Xylophone/Color+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  Color+Extensions.swift
+//  Xylophone
+//
+//  Created by Ryan Rouxinol on 13/07/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let redAsset = UIColor(named: "Xylo_Red")
+    static let blueAsset = UIColor(named: "Xylo_Blue")
+    static let orangeAsset = UIColor(named: "Xylo_Orange")
+    static let purpleAsset = UIColor(named: "Xylo_Purple")
+    static let greenAsset = UIColor(named: "Xylo_Green")
+    static let sblueAsset = UIColor(named: "Xylo_SBlue")
+    static let yellowAsset = UIColor(named: "Xylo_Yellow")
+}
+
--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private lazy var backgroundView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "background")
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
+        
     }
 
-
+    private func setupView(){
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy(){
+        view.addSubview(backgroundView)
+        
+    }
+    
+    private func setConstraints(){
+        NSLayoutConstraint.activate([
+            backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+    }
 }
 
